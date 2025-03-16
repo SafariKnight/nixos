c9f399e2d7dbd527202abae1637c9ff2e313d324
Kareem Hisham <safariknight2@gmail.com>
2025-03-16 22:45:51 +0200

    cleanup: remove all unused files

--- homes/kareem/cmdline/_zellij.nix
+++
@@ -1,5 +0,0 @@
-{ config, ... }:
-{
-  programs.zellij.enable = true;
-  xdg.configFile.zellij.source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos-config/dotfiles/zellij/";
-}
