874c9580787107d274ada30352243a93660922e7
Kareem Hisham <safariknight2@gmail.com>
2026-01-18 12:06:54 +0200

    feat(knv): vtsls config

--- pkgs/special/knv/lua/plugins/lspconfig.lua
+++ pkgs/special/knv/lua/plugins/lspconfig.lua
@@ -40,7 +40,35 @@ local opts = {
   laravel_ls = {},
   -- phpactor = {},
   cssls = {},
-  vtsls = {},
+  vtsls = {
+    settings = {
+      typescript = {
+        updateImportsOnFileMove = { enabled = "always" },
+        inlayHints = {
+          enumMemberValues = { enabled = true },
+          functionLikeReturnTypes = { enabled = true },
+          parameterNames = { enabled = "all" },
+          parameterTypes = { enabled = true },
+          propertyDeclarationTypes = { enabled = true },
+          variableTypes = { enabled = true },
+        },
+      },
+      javascript = {
+        updateImportsOnFileMove = { enabled = "always" },
+        inlayHints = {
+          enumMemberValues = { enabled = true },
+          functionLikeReturnTypes = { enabled = true },
+          parameterNames = { enabled = "literals" },
+          parameterTypes = { enabled = true },
+          propertyDeclarationTypes = { enabled = true },
+          variableTypes = { enabled = true },
+        },
+      },
+      vtsls = {
+        enableMoveToFileCodeAction = true,
+      },
+    },
+  },
   svelte = {},
   angularls = {},
   tailwindcss = {},
